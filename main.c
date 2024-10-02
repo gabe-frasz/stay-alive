2cce56aa97b173886e6b6803a5b1a74b778dbc1c
Gabriel Frasão <gabrielvitor.frasao@gmail.com>
2024-10-02 01:57:19 -0300

    add map and character images, make camera move instead of the character

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <allegro5/allegro5.h>
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_image.h>
 #include <stdbool.h>
 #include <stdio.h>
 
@@ -10,16 +11,12 @@ const int DISPLAY_HEIGHT = 720;
 
 void must_init(bool test, char* description);
 
-typedef struct {
-    float x, y, size, correct_x, correct_y, id;
-    bool is_correct;
-} item;
-
 int main() {
     must_init(al_init(), "allegro");
     must_init(al_init_primitives_addon(), "primitives");
     must_init(al_install_keyboard(), "keyboard");
     must_init(al_install_mouse(), "mouse");
+    must_init(al_init_image_addon(), "image");
 
     ALLEGRO_TIMER* timer = al_create_timer(1.0 / FPS);
     must_init(timer, "timer");
@@ -35,40 +32,39 @@ int main() {
     al_register_event_source(queue, al_get_timer_event_source(timer));
     al_register_event_source(queue, al_get_mouse_event_source());
 
+    ALLEGRO_BITMAP* map_img = al_load_bitmap("images/map.jpg");
+    must_init(map_img, "map image");
+    ALLEGRO_BITMAP* char_front_img = al_load_bitmap("images/parado_frente.png");
+    must_init(char_front_img, "character front image");
+    ALLEGRO_BITMAP* char_walk1_front_img = al_load_bitmap("images/andando_frente1.png");
+    must_init(char_walk1_front_img, "character walking 1 front image");
+    ALLEGRO_BITMAP* char_walk2_front_img = al_load_bitmap("images/andando_frente2.png");
+    must_init(char_walk2_front_img, "character walking 2 front image");
+    ALLEGRO_BITMAP* char_back_img = al_load_bitmap("images/parado_tras.png");
+    must_init(char_back_img, "character back image");
+    ALLEGRO_BITMAP* char_walk1_back_img = al_load_bitmap("images/andando_tras1.png");
+    must_init(char_walk1_back_img, "character walking 1 back image");
+    ALLEGRO_BITMAP* char_walk2_back_img = al_load_bitmap("images/andando_tras2.png");
+    must_init(char_walk2_back_img, "character walking 2 back image");
+    ALLEGRO_BITMAP* char_left_img = al_load_bitmap("images/parado_esquerda.png");
+    must_init(char_left_img, "character left image");
+    ALLEGRO_BITMAP* char_walk1_left_img = al_load_bitmap("images/andando_esquerda1.png");
+    must_init(char_walk1_left_img, "character walking 1 left image");
+    ALLEGRO_BITMAP* char_walk2_left_img = al_load_bitmap("images/andando_esquerda2.png");
+    must_init(char_walk2_left_img, "character walking 2 left image");
+    ALLEGRO_BITMAP* char_right_img = al_load_bitmap("images/parado_direita.png");
+    must_init(char_right_img, "character right image");
+    ALLEGRO_BITMAP* char_walk1_right_img = al_load_bitmap("images/andando_direita1.png");
+    must_init(char_walk1_right_img, "character walking 1 right image");
+    ALLEGRO_BITMAP* char_walk2_right_img = al_load_bitmap("images/andando_direita2.png");
+    must_init(char_walk2_right_img, "character walking 2 right image");
+    ALLEGRO_BITMAP* character = char_front_img;
+
     bool done = false;
     bool redraw = true;
     ALLEGRO_EVENT event;
 
-    float player_x = 100, player_y = 100, objective_x = 1100, objective_y = 600;
-    bool is_inside_objective = false;
-    char* msg = "";
-    int bg_color[3] = { 0, 0, 0 }, text_color[3] = { 255, 255, 255 },
-        game_challenge = 0;
-    item items[4] = { {.x = 20,
-                      .y = 650,
-                      .size = 40,
-                      .correct_x = 140,
-                      .correct_y = 200,
-                      .id = 1},
-                     {.x = 100,
-                      .y = 590,
-                      .size = 90,
-                      .correct_x = 20,
-                      .correct_y = 150,
-                      .id = 2},
-                     {.x = 400,
-                      .y = 570,
-                      .size = 75,
-                      .correct_x = 500,
-                      .correct_y = 290,
-                      .id = 3},
-                     {.x = 600,
-                      .y = 595,
-                      .size = 120,
-                      .correct_x = 800,
-                      .correct_y = 150,
-                      .id = 4} };
-    int selected_item = 0;
+    float player_x = DISPLAY_WIDTH/2.0, player_y = DISPLAY_HEIGHT/2.0, map_x = -1000, map_y = -1000;
 
     al_start_timer(timer);
     while (1) {
@@ -76,75 +72,61 @@ int main() {
 
         switch (event.type) {
         case ALLEGRO_EVENT_TIMER:
-            if (game_challenge == 0) {
-                if (player_x == objective_x && player_y == objective_y) {
-                    is_inside_objective = true;
-                    msg = "Aperte ENTER para continuar";
-                }
-                else {
-                    is_inside_objective = false;
-                    msg = "Mova o jogador para a área branca usando as setas";
-                }
-            }
-            if (game_challenge == 1) {
-                bg_color[0] = 255;
-                bg_color[1] = 255;
-                bg_color[2] = 255;
-                text_color[0] = 0;
-                text_color[1] = 0;
-                text_color[2] = 0;
-
-                msg = "Agora use o mouse para clicar nos objetos e posicioná-los "
-                    "corretamente";
-
-                for (int i = 0; i < 4; i++) {
-                    if (!items[i].is_correct) {
-                        break;
-                    }
-
-                    msg = "Parabéns! Você concluiu o desafio!";
-                }
-            }
-
-            redraw = true;
+              redraw = true;
             break;
         case ALLEGRO_EVENT_KEY_CHAR:
+            float cycle_timer = ((float)al_get_timer_count(timer)/FPS - al_get_timer_count(timer)/FPS) * FPS;
+        
             if (event.keyboard.keycode == ALLEGRO_KEY_UP) {
-                player_y -= 20;
+                map_y += 5;
+                if (cycle_timer >= 0 && cycle_timer <= 7.5 || cycle_timer >= 15 && cycle_timer <= 22.5) {
+                    character = char_back_img;
+                }
+                if (cycle_timer > 7.5 && cycle_timer <= 15) {
+                    character = char_walk1_back_img;
+                }
+                if (cycle_timer > 22.5 && cycle_timer <= 30) {
+                    character = char_walk2_back_img;
+                }
             }
             if (event.keyboard.keycode == ALLEGRO_KEY_DOWN) {
-                player_y += 20;
+                map_y -= 5;
+                if (cycle_timer >= 0 && cycle_timer <= 7.5 || cycle_timer >= 15 && cycle_timer <= 22.5) {
+                    character = char_front_img;
+                }
+                if (cycle_timer > 7.5 && cycle_timer <= 15) {
+                    character = char_walk1_front_img;
+                }
+                if (cycle_timer > 22.5 && cycle_timer <= 30) {
+                    character = char_walk2_front_img;
+                }
             }
             if (event.keyboard.keycode == ALLEGRO_KEY_LEFT) {
-                player_x -= 20;
+                map_x += 5;
+                if (cycle_timer >= 0 && cycle_timer <= 7.5 || cycle_timer >= 15 && cycle_timer <= 22.5) {
+                    character = char_left_img;
+                }
+                if (cycle_timer > 7.5 && cycle_timer <= 15) {
+                    character = char_walk1_left_img;
+                }
+                if (cycle_timer > 22.5 && cycle_timer <= 30) {
+                    character = char_walk2_left_img;
+                }
             }
             if (event.keyboard.keycode == ALLEGRO_KEY_RIGHT) {
-                player_x += 20;
-            }
-
-            if (event.keyboard.keycode == ALLEGRO_KEY_ENTER) {
-                if (is_inside_objective)
-                    game_challenge = 1;
+                map_x -= 5;
+                if (cycle_timer >= 0 && cycle_timer <= 7.5 || cycle_timer >= 15 && cycle_timer <= 22.5) {
+                    character = char_right_img;
+                }
+                if (cycle_timer > 7.5 && cycle_timer <= 15) {
+                    character = char_walk1_right_img;
+                }
+                if (cycle_timer > 22.5 && cycle_timer <= 30) {
+                    character = char_walk2_right_img;
+                }
             }
             break;
         case ALLEGRO_EVENT_MOUSE_BUTTON_UP:
-            for (int i = 0; i < 4; i++) {
-                if (event.mouse.x >= items[i].x &&
-                    event.mouse.x <= items[i].x + items[i].size &&
-                    event.mouse.y >= items[i].y &&
-                    event.mouse.y <= items[i].y + items[i].size) {
-                    selected_item = items[i].id;
-                }
-
-                if (event.mouse.x >= items[i].correct_x &&
-                    event.mouse.x <= items[i].correct_x + items[i].size &&
-                    event.mouse.y >= items[i].correct_y &&
-                    event.mouse.y <= items[i].correct_y + items[i].size) {
-                    if (selected_item == items[i].id) {
-                        items[i].is_correct = true;
-                    }
-                }
-            }
             break;
         case ALLEGRO_EVENT_DISPLAY_CLOSE:
             done = true;
@@ -155,46 +137,27 @@ int main() {
             break;
 
         if (redraw && al_is_event_queue_empty(queue)) {
-            al_clear_to_color(al_map_rgb(bg_color[0], bg_color[1], bg_color[2]));
-            al_draw_text(font,
-                al_map_rgb(text_color[0], text_color[1], text_color[2]), 0,
-                0, 0, msg);
-
-            switch (game_challenge) {
-            case 0:
-                al_draw_filled_rectangle(objective_x, objective_y, objective_x + 100,
-                    objective_y + 100,
-                    al_map_rgb(255, 255, 255)); // objective
-                al_draw_filled_rectangle(player_x, player_y, player_x + 100,
-                    player_y + 100,
-                    al_map_rgb(255, 0, 0)); // player
-                break;
-            case 1:
-                for (int i = 0; i < 4; i++) {
-                    if (items[i].is_correct) {
-                        al_draw_filled_rectangle(items[i].correct_x, items[i].correct_y,
-                            items[i].correct_x + items[i].size,
-                            items[i].correct_y + items[i].size,
-                            al_map_rgb(0, 0, 0));
-                    }
-                    else {
-                        al_draw_filled_rectangle(items[i].correct_x, items[i].correct_y,
-                            items[i].correct_x + items[i].size,
-                            items[i].correct_y + items[i].size,
-                            al_map_rgb(220, 220, 220));
-                        al_draw_filled_rectangle(
-                            items[i].x, items[i].y, items[i].x + items[i].size,
-                            items[i].y + items[i].size, al_map_rgb(0, 0, 0));
-                    }
-                }
-                break;
-            }
+            al_clear_to_color(al_map_rgb(0, 0, 0));
+            al_draw_bitmap(map_img, map_x, map_y, 0);
+            al_draw_bitmap(character, player_x, player_y, 0);
 
             al_flip_display();
-
             redraw = false;
         }
     }
+    al_destroy_bitmap(map_img);
+    al_destroy_bitmap(char_front_img);
+    al_destroy_bitmap(char_walk1_front_img);
+    al_destroy_bitmap(char_walk2_front_img);
+    al_destroy_bitmap(char_back_img);
+    al_destroy_bitmap(char_walk1_back_img);
+    al_destroy_bitmap(char_walk2_back_img);
+    al_destroy_bitmap(char_left_img);
+    al_destroy_bitmap(char_walk1_left_img);
+    al_destroy_bitmap(char_walk2_left_img);
+    al_destroy_bitmap(char_right_img);
+    al_destroy_bitmap(char_walk1_right_img);
+    al_destroy_bitmap(char_walk2_right_img);
 
     al_destroy_font(font);
     al_destroy_display(disp);
