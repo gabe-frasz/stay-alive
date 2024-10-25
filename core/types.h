d3fe117ab5ca5c555c1ae340669fa11bbc68bc5f
gabriel.vsfrasao <gabriel.vsfrasao@CAS674950A113D>
2024-10-24 21:28:27 -0300

    insert first challenge into context

--- core/types.h
+++ core/types.h
@@ -31,8 +31,17 @@ typedef struct {
 } Coordinate;
 
 typedef struct {
+    Coordinate initial_position, correct_position;
+    int id, width, height;
+} Selectable_Object;
+
+typedef struct {
     float x1, x2, y1, y2;
 } Challenge_Area;
+
+typedef struct {
+    Selectable_Object selectable_objects[5];
+} Challenge_1;
 
 enum Game_State {
     MENU,
@@ -49,6 +58,7 @@ typedef struct {
     ALLEGRO_EVENT event;
     Images imgs;
     Coordinate player, map;
+    Challenge_1 c1;
     Challenge_Area challenges_areas[5];
     int challenge_index, life_counter, hunger_counter;
     enum Game_State state;
